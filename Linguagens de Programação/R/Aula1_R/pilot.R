6562ca4e34d814696876d9fe97ca94147317dad4
JOAO BATISTA DE ANDRADE JUNIOR <jbajunior@inf.ufpr.br>
2024-05-03 22:09:59 -0300

    Inserindo arquivos da aula de R

---
+++ Linguagens de Programação/R/Aula1_R/pilot.R
@@ -0,0 +1,38 @@
+2 + 2
+
+x <- 2
+y <- 3
+
+x + y
+
+ls()
+rm(x)
+
+rm(list= ls())
+
+search()
+
+ls("package:datasets")
+
+WorldPhones <- 10986
+WorldPhones
+
+Titanic
+
+getwd()
+list.files()
+
+-----------------------------------------------------------------
+  
+base::install.packages("ggplot2")
+BiocManager::install()
+
+library(ggplot2)
+require(ggplot2)
+-----------------------------------------------------------------
+.libPaths()
+
+help(mean)
+??mean
+?install.packages
+RSiteSearch("mean")
